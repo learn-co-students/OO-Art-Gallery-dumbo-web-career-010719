d3b8a014c7ae1e5e7c00269c6761f1397829dd19
github username <github email address>
2019-01-15 20:08:57 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,42 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+  @@total_artits_years = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@total_artits_years << years_experience
+    @@all << self
+  end
+
+  def self.all
+  	@all
+  end
+
+  def paintings # Returns an `array` all the paintings by an artist
+  	Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries # Returns an `array` of all the galleries that an artist has paintings in
+  	paintings.map { |painting| painting.gallery }
+  end
+
+  def cities # Return an `array` of all cities that an artist has paintings in
+  	paintings.map { |painting| painting.gallery }.map { |gallery| gallery.city }
+  end
+
+  def self.total_experience # Returns an `integer` that is the total years of experience of all artists
+  	@@total_artits_years.reduce(0, :+)
+  end
+
+  def self.most_prolific # Returns an `instance` of the artist with the highest amount of paintings per year of experience
+  	@@all.max_by { |artist| @@all.count(artist) }
+  end
+
+  def create_painting(title, price, gallery) # Given the arguments of `title`, `price` and `gallery`, creates a new painting belonging to that artist
+  	Painting.new(title, price, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,13 @@
 require_relative '../config/environment.rb'
 
 
+jim = Artist.new("Jim", 12)
+bob = Artist.new("Bob", 7)
 
+no1_gallery = Gallery.new("No 1 Gallery", "New York")
+
+rainbow = Painting.new("rainbow", 100, no1_gallery, bob)
+pony = Painting.new("pony", 200, no1_gallery, jim)
 
 binding.pry
 
