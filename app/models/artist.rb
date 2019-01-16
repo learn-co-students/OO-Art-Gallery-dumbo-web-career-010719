e34a795232d3fa7c75f43ff005743aa2ba9e1855
edwinlin <edwinlin@gmail.com>
2019-01-15 21:25:01 -0500

    50 minutes

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,49 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+  	@@all
+  end
+
+  def paintings
+  	Painting.all.select do |painting|
+  		painting.artist == self
+  	end
+  end
+
+  def galleries
+  	paintings.map do |painting|
+  		painting.gallery
+  	end
+  end
+
+  def cities
+  	galleries.map do |gallery|
+  		gallery.city
+  	end
+  end
+
+  def self.total_experience
+  	Artist.all.inject(0) {|sum, artist| sum + artist.years_experience}
+  end
+
+  def self.most_prolific
+  	prolific = {}
+  	@@all.each do |artist|
+  		prolific[artist] = artist.paintings.count.to_f/artist.years_experience.to_f
+  	end
+  	prolific.max_by{|k,v| v}[0]
+  end
+
+  def create_painting(title, price, gallery)
+  	Painting.new(title, price, gallery, self)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,23 @@
 require_relative '../config/environment.rb'
 
 
+artist1 = Artist.new("Art", 25)
+artist2 = Artist.new("Bob", 10)
+artist3 = Artist.new("Chuck", 15)
+
+gallery1 = Gallery.new("Guug", "NY")
+gallery2 = Gallery.new("Smith", "GA")
+gallery3 = Gallery.new("Berl", "Berlin")
+gallery4 = Gallery.new("Wonk", "AZ")
+
+painting1 = Painting.new("No", 100, gallery1, artist1)
+painting2 = Painting.new("Seeking", 25, gallery2, artist1)
+painting3 = Painting.new("Chair", 10, gallery3, artist1)
+painting4 = Painting.new("Happy", 150, gallery2, artist2)
+painting5 = Painting.new("Mis", 35, gallery3, artist3)
+painting6 = Painting.new("Wall", 35, gallery4, artist2)
+painting7 = Painting.new("Floor", 35, gallery4, artist1)
+painting8 = Painting.new("Cup", 35, gallery1, artist3)
 
 
 binding.pry
